f0910ed7ef51c7be286bca6c26df1eb7d5a145cd
Pedro Lucas <pedrolucas1101@proton.me>
2023-10-16 20:01:44 -0300

    EU ESQUECI DE COMITAR ME MATAREI

--- deddo/App.js
+++ deddo/App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Modal } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import Janela from './components/janela';
+import Slider from '@react-native-community/slider';
 
 export default function App() {
+  const [size, setSize] = useState(10);
+  const [password, setPassword] = useState("");
+  const [visiblee, setVisiblee] = useState(false);
+  
+  function passwordGenerate(){
+    let pass = "";
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()_+";
+    for (let i = 0, n = charset.length; i < size; i++) {
+      pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    setPassword(pass);
+    setVisiblee(true);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Gerador de Senhas</Text>
+      <Image source={require("./src/assets/programmingPepe.jpeg")} style={styles.pepe} />
+      
+      <Text style={styles.caracteres}>{size} CARACTERES</Text>
+      <View>
+        <Slider 
+        style={styles.sliderSenha}
+        minimumValue={6} 
+        maximumValue={24}
+        maximumTrackTintColor="#ff0000"
+        minimumTrackTintColor="#00ff00"
+        value={size}
+        onValueChange={ (value) => setSize(value.toFixed(0))}/>
+      </View>
+      <View style={{marginTop: 20}}>
+        <TouchableOpacity style={styles.button}  onPress={passwordGenerate}>
+          <Text style={styles.gerarsenha}>GERAR SENHA</Text>
+        </TouchableOpacity>
+      </View>
+      
+      <Modal visible={visiblee} animationType="fade" onDismiss={() => setVisiblee(false)}>
+        <Janela senha={password} />
+      </Modal>
+    <Text>senha {password}</Text>
     </View>
   );
 }
@@ -13,8 +53,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e0e0e0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  pepe: {
+    width: 200,
+    height: 200,
+    marginBottom: 50
+  },
+  sliderSenha: {
+    backgroundColor: "#F0F0F0",
+    padding: 10,
+    borderRadius: 10,
+    width: 300,
+    marginTop: 20,
+    marginBottom: 10
+  },
+  button: {
+    backgroundColor: "#fd7e14",
+    padding: 15,
+    borderRadius: 10,
+    width: 300,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  titulo: {
+    fontSize: 30,
+    color: "#000",
+    marginBottom: 50
+  },
+  caracteres: {
+    fontSize: 20,
+    color: "#000"
+  },
+  gerarsenha: {
+    fontSize: 20,
+    color: "#FFF"
+  }
+
 });
---
+++ deddo/components/janela.js
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export default function Janela(props) {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titulo}>A SENHA GERADA É:</Text>
+            <Text style={styles.texto}>{props.senha}</Text>
+        </View>
+    
+
+);}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'rgba(24,24,24,0.6)',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    titulo: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    texto: {
+        fontSize: 16,
+    },
+});
